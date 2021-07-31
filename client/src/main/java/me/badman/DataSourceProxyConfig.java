42962bbb147c739b54002157c8f45fe686bff6c7
p-eve <peve@163.com>
2021-08-01 03:27:35 +0800

    add seata

--- client/src/main/java/me/badman/DataSourceProxyConfig.java
+++ client/src/main/java/me/badman/DataSourceProxyConfig.java
@@ -8,34 +8,29 @@ import org.mybatis.spring.transaction.SpringManagedTransactionFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class DataSourceConfiguration {
+public class DataSourceProxyConfig {
 
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource druidDataSource(){
+    public DataSource dataSource() {
         DruidDataSource druidDataSource = new DruidDataSource();
         return druidDataSource;
     }
 
-    @Primary
-    @Bean("dataSource")
-    public DataSourceProxy dataSource(DataSource druidDataSource){
-        return new DataSourceProxy(druidDataSource);
-    }
-
     @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSourceProxy dataSourceProxy)throws Exception{
+    public SqlSessionFactory SqlSessionFactoryBean(DataSource dataSource)throws Exception{
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSourceProxy);
+        sqlSessionFactoryBean.setDataSource(new DataSourceProxy(dataSource));
         sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver()
                 .getResources("classpath*:/mapper/*.xml"));
         sqlSessionFactoryBean.setTransactionFactory(new SpringManagedTransactionFactory());
-        return sqlSessionFactoryBean.getObject();
+        SqlSessionFactory factory=sqlSessionFactoryBean.getObject();
+        factory.getConfiguration().setMapUnderscoreToCamelCase(true);
+        return factory;
     }
 }
